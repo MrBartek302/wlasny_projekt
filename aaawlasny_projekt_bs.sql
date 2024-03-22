4b5df3defb1ac1e3a02c3fdc9db02571c23702db
MrBartek302 <126457666+MrBartek302@users.noreply.github.com>
2024-03-22 17:03:00 +0100

    1

--- aaawlasny_projekt_bs.sql
+++ aaawlasny_projekt_bs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Mar 2024, 15:25
--- Wersja serwera: 10.4.14-MariaDB
--- Wersja PHP: 7.4.9
+-- Czas generowania: 22 Mar 2024, 17:02
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `uprawnienia` (
   `ID_upr` int(11) NOT NULL,
-  `nazwa_upr` text COLLATE utf32_polish_ci NOT NULL
+  `nazwa_upr` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_polish_ci;
 
 --
@@ -49,9 +49,9 @@ INSERT INTO `uprawnienia` (`ID_upr`, `nazwa_upr`) VALUES
 
 CREATE TABLE `uzytkownicy` (
   `ID` int(11) NOT NULL,
-  `login` text COLLATE utf32_polish_ci NOT NULL,
-  `pass` text COLLATE utf32_polish_ci NOT NULL,
-  `upr` text COLLATE utf32_polish_ci NOT NULL
+  `login` text NOT NULL,
+  `pass` text NOT NULL,
+  `upr` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_polish_ci;
 
 --
@@ -63,7 +63,8 @@ INSERT INTO `uzytkownicy` (`ID`, `login`, `pass`, `upr`) VALUES
 (2, 'user', '21232f297a57a5a743894a0e4a801fc3', 'user'),
 (3, 'pracownik', '21232f297a57a5a743894a0e4a801fc3', 'pracownik'),
 (4, 'gfdg', 'ae65ca6fdbbead5105c0254745371bba', 'user'),
-(5, '111', '698d51a19d8a121ce581499d7b701668', 'user');
+(5, '111', '698d51a19d8a121ce581499d7b701668', 'user'),
+(6, 'ds', '698d51a19d8a121ce581499d7b701668', 'user');
 
 -- --------------------------------------------------------
 
@@ -73,8 +74,8 @@ INSERT INTO `uzytkownicy` (`ID`, `login`, `pass`, `upr`) VALUES
 
 CREATE TABLE `wydarzenia` (
   `ID` int(11) NOT NULL,
-  `nazwa_wyd` text COLLATE utf32_polish_ci NOT NULL,
-  `opis_wyd` text COLLATE utf32_polish_ci NOT NULL,
+  `nazwa_wyd` text NOT NULL,
+  `opis_wyd` text NOT NULL,
   `data_wyd` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_polish_ci;
 
@@ -93,9 +94,16 @@ INSERT INTO `wydarzenia` (`ID`, `nazwa_wyd`, `opis_wyd`, `data_wyd`) VALUES
 
 CREATE TABLE `zainteresowania` (
   `ID` int(11) NOT NULL,
-  `uzytkownik` text COLLATE utf32_polish_ci NOT NULL,
+  `uzytkownik` text NOT NULL,
   `id_wydarzenia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_polish_ci;
+
+--
+-- Zrzut danych tabeli `zainteresowania`
+--
+
+INSERT INTO `zainteresowania` (`ID`, `uzytkownik`, `id_wydarzenia`) VALUES
+(1, 'user', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -126,7 +134,7 @@ ALTER TABLE `zainteresowania`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
@@ -139,7 +147,7 @@ ALTER TABLE `uprawnienia`
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `wydarzenia`
@@ -151,7 +159,7 @@ ALTER TABLE `wydarzenia`
 -- AUTO_INCREMENT dla tabeli `zainteresowania`
 --
 ALTER TABLE `zainteresowania`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
